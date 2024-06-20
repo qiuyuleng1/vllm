a5d95e6eaf3530357c52687deac35ba05bb1296e
qiuyuleng1 <qiuyu.leng@intel.com>
2024-06-20 17:06:16 +0000

    support 2nd+ token pipeline parallel

--- vllm/entrypoints/slave.py
+++ vllm/entrypoints/slave.py
@@ -4,6 +4,7 @@ import xfastertransformer
 
 # for logits send
 from mpi4py import MPI
+import numpy as np
 
 def parse_args():
     parser = make_arg_parser()
@@ -27,27 +28,25 @@ if __name__ == "__main__":
     comm = MPI.COMM_WORLD
 
     while True:
-        print("model rank, color, section", model.rank, model.color, model.section)
-        if model.rank == 0:
-            input_tokens= [[1, 450, 6673, 310, 278, 3303, 3900, 338]]
-            xft_seq_ids = None
-            xft_max_lens = [4]
-            xft_seq_ids = model.set_input_cb(input_tokens, xft_seq_ids, xft_max_lens).tolist()
-            logits = model.forward_cb()
-            print("logits", logits)
-            if model.color == 1: #TODO 这个判断咋写
-                # tmp_tensor = torch.Tensor()
-                # tmp_tensor = tmp_tensor.new_full((3, 2), 7)
-                # tmp = 512
-                print("开始发送数据")
-                comm.send(logits, dest=0)  # list怎样用做tag？list里面的挨着发？
-                print("数据已发送")
-            model.free_seqs(xft_seq_ids)
-        else:
+        with open("output.txt", "a") as f:  
+            f.write("=============slave===============")
+            f.flush()
+            f.write(f"model rank: {model.rank}, color: {model.color}, section: {model.section}")
+            f.flush()
             model.set_input_cb()
-            model.forward_cb()
-            # 不free mpi会报错
-            model.free_seqs()
+            f.write("finish set_input_cb")
+            f.flush()
+            logits = model.forward_cb()
+            f.write("\n logits: " + np.array2string(logits.detach().cpu().numpy()) + "\n") 
+
+            if model.color == 1:
+                print_info = "开始发送数据"
+                f.write(print_info + "\n")  
+
+                comm.send(logits, dest=0)  
+
+                print_info = "数据已发送"
+                f.write(print_info + "\n")  #直接写入你要的字符串
         
         
         
